b33896ef530388f27a7b53ef24af215594af917c
Nghia Nguyen (Mitchell) <nghia6a1@gmail.com>
2026-01-20 11:54:26 +1000

    split script into 2
    one script for RNA-seq, one for qPCR data.

--- scripts/6_plot_box_mndata_qpcr.R
+++ scripts/6_plot_box_mndata_qpcr.R
@@ -3,8 +3,7 @@ library("tidyverse")
 library("ggpattern")
 library("ggsignif")
 
-# MN qPCR data
-## Load data
+# Load data
 mndata_quant_big <- readRDS(
   file = here(
     "output",
@@ -18,7 +17,7 @@ mndata_fit_contrasts <- readRDS(
   )
 )
 
-## Set contrasts of interest
+# Set contrasts of interest
 mndata_contrasts_interest_response <- c(
   "Hep_hMSC_P5",
   "Hep_hMSC_P13",
@@ -28,8 +27,8 @@ mndata_contrasts_interest_response <- c(
   "Hep_SH"
 )
 
-## Create list of topTables for boxplots and volcano plots
-## Unclipped for boxplots
+# Create list of topTables for boxplots and volcano plots
+# Unclipped for boxplots
 mndata_contrasts <- seq_len(ncol(mndata_fit_contrasts$contrasts)) %>%
   setNames(colnames(mndata_fit_contrasts$contrasts))
 
@@ -44,8 +43,8 @@ mndata_toptable <- mndata_contrasts %>%
       )
   })
 
-## Define functions for drawing boxplots
-### Draw boxplot hMSC response to heparin
+# Define functions for drawing boxplots
+## Draw boxplot hMSC response to heparin
 mndata_plot_goi_hMSC_response <- function(gene_sel) {
   # Get table containing dCt for hMSC
   quant_small <- mndata_quant_big %>%
@@ -148,7 +147,7 @@ mndata_plot_goi_hMSC_response <- function(gene_sel) {
   return(plot)
 }
 
-### Draw boxplot cell type differences in baseline
+## Draw boxplot cell type differences in baseline
 mndata_plot_cellpops_baseline <- function(gene_sel) {
   # Get table containing dCt for control only
   quant_small <- mndata_quant_big %>%
@@ -272,7 +271,7 @@ mndata_plot_cellpops_baseline <- function(gene_sel) {
   return(plot)
 }
 
-### Draw boxplot cell type differences in response (log exprsn)
+## Draw boxplot cell type differences in response (log exprsn)
 mndata_plot_cellpops_response <- function(gene_sel) {
   # Get table containing dCt for control only
   quant_small <- mndata_quant_big %>%
@@ -399,7 +398,7 @@ mndata_plot_cellpops_response <- function(gene_sel) {
   return(plot)
 }
 
-### Draw boxplot cell type differences in response (logFC)
+## Draw boxplot cell type differences in response (logFC)
 mndata_plot_cellpops_response_lfc <- function(gene_sel) {
   # Get table containing logFC
   toptable_sel <- mndata_toptable %>%
@@ -525,7 +524,7 @@ mndata_plot_cellpops_response_lfc <- function(gene_sel) {
   return(plot)
 }
 
-### Draw boxplot cell type differences in response (linexp)
+## Draw boxplot cell type differences in response (linexp)
 mndata_plot_cellpops_response_linexp <- function(gene_sel) {
   # Get normalisation factor for dct
   # i.e. Get geomean of hMSC P5 Ctrl, divide all dct by geomean
@@ -633,7 +632,7 @@ mndata_plot_cellpops_response_linexp <- function(gene_sel) {
   return(plot)
 }
 
-## Draw list of boxplots
+# Draw list of boxplots
 mndata_plots <- list(
   hMSC_response = mndata_genes %>%
     map(\(gene) mndata_plot_goi_hMSC_response(gene)),
@@ -648,159 +647,6 @@ mndata_plots <- list(
 ) %>%
   map(\(plotlist) set_names(plotlist, mndata_genes))
 
-# MN txome data
-## Define conditions of interest
-txome_conditions_interest <- c(
-  "P5D5Untreated",
-  "P13D5Untreated",
-  "P5D5Treated",
-  "P13D5Treated"
-)
-
-## Load data
-txome_quant_deseq2_batchcor <- readRDS(
-  file = here::here(
-    "input",
-    "txome",
-    "quant_deseq2_batchcor_nofilter.RDS"
-  )
-)
-txome_results <- readRDS(
-  file = here::here(
-    "input",
-    "txome",
-    "results_deseq2_nofilter.RDS"
-  )
-)
-
-## Subset data based on conditions and genes
-txome_quant_small <- txome_quant_deseq2_batchcor %$%
-  .[, colData(.)$condition_ID %in% txome_conditions_interest] %$%
-  .[, colData(.)$cell_line %in% c("hMSC-21558")]
-
-txome_genes_sel <- txome_quant_small %>%
-  rowRanges() %>%
-  as.data.frame() %>%
-  filter(symbol %in% mndata_genes) %>%
-  arrange(match(gene_name, mndata_genes))
-
-## Readjust DESeq results to include only GOIs
-txome_results_subset <- txome_results %>%
-  map(\(x) {
-    x <- x[rownames(x) %in% txome_genes_sel$gene_id, ]
-    x$padj <- p.adjust(x$pvalue, method = "BH")
-    return(x)
-  })
-
-## Define functions for drawing boxplots
-### Draw boxplot hMSC response to heparin (txome)
-txome_plot_goi <- function(gene_sel, gene_name) {
-  # Get table containing normalised gene counts
-  gene_counts <- txome_quant_small %>%
-    plotCounts(
-      gene = gene_sel,
-      intgroup = c("condition_ID", "Treatment"),
-      returnData = TRUE
-    ) %>%
-    droplevels()
-
-  # Get position to start drawing signif bars
-  y_position <- log10(max(gene_counts$count) * 1.1)
-
-  # Define position of bars and numbers manually
-
-  # Plot data
-  plot <- gene_counts %>%
-    ggplot(aes(
-      x = condition_ID,
-      y = count
-    )) +
-    geom_boxplot(
-      aes(
-        colour = condition_ID
-      ),
-      outliers = FALSE
-    ) +
-    geom_jitter(
-      aes(
-        fill = condition_ID,
-        shape = Treatment
-      ),
-      colour = "black",
-      size = 1.2,
-      stroke = 0.5,
-      alpha = 0.5
-    ) +
-    geom_signif(
-      comparisons = list(
-        c("P5D5Untreated", "P5D5Treated"),
-        c("P5D5Untreated", "P13D5Untreated"),
-        c("P13D5Untreated", "P13D5Treated")
-      ),
-      annotation = c(2, 21, 6) %>%
-        map(\(coef) txome_results_subset[[coef]][gene_sel, ]$padj) %>%
-        unlist() %>%
-        stars_pval(),
-      y_position = y_position %>%
-        rep(3),
-      textsize = 3,
-      step_increase = 0.2
-    ) +
-    scale_colour_manual(
-      values = palette_merge[c(1, 2, 5, 6)],
-      labels = c(
-        "P5D5Untreated" = "P+5 D5 Control",
-        "P5D5Treated" = "P+5 D5 Heparin",
-        "P13D5Untreated" = "P+13 D5 Control",
-        "P13D5Treated" = "P+13 D5 Heparin"
-      )
-    ) +
-    scale_fill_manual(
-      values = palette_merge[c(1, 2, 5, 6)],
-      labels = c(
-        "P5D5Untreated" = "P+5 D5 Control",
-        "P5D5Treated" = "P+5 D5 Heparin",
-        "P13D5Untreated" = "P+13 D5 Control",
-        "P13D5Treated" = "P+13 D5 Heparin"
-      )
-    ) +
-    scale_shape_manual(
-      values = c(
-        "Untreated" = 21,
-        "Treated" = 24
-      )
-    ) +
-    scale_x_discrete(
-      labels = c(
-        "P5D5Untreated" = "P+5 D5 Control",
-        "P5D5Treated" = "P+5 D5 Heparin",
-        "P13D5Untreated" = "P+13 D5 Control",
-        "P13D5Treated" = "P+13 D5 Heparin"
-      )
-    ) +
-    scale_y_log10(expand = expansion(0, 0.15)) +
-    theme_classic() +
-    ggtitle(label = gene_name) +
-    theme(
-      axis.text.x = element_blank(),
-      axis.ticks.x = element_blank()
-    ) +
-    ylab("Norm. counts")
-
-  # Return object
-  return(plot)
-}
-
-## Draw plots
-### Boxplots of norm counts
-txome_plots_goi <- txome_genes_sel %$%
-  map2(
-    .$gene_id,
-    .$gene_name,
-    \(x, y) txome_plot_goi(x, y)
-  ) %>%
-  set_names(txome_genes_sel$gene_name)
-
 # Save data
 saveRDS(
   mndata_plots,
@@ -809,15 +655,5 @@ saveRDS(
     "plots_box",
     "data",
     "mndata_plots.rds"
-  )
-)
-
-saveRDS(
-  txome_plots_goi,
-  here::here(
-    "output",
-    "plots_box",
-    "data",
-    "txome_plots_goi.rds"
   )
 )
---
+++ scripts/6_plot_box_mndata_rnaseq.R
@@ -0,0 +1,163 @@
+# Import packages
+
+# Define conditions of interest
+txome_conditions_interest <- c(
+  "P5D5Untreated",
+  "P13D5Untreated",
+  "P5D5Treated",
+  "P13D5Treated"
+)
+# Load data
+txome_quant_deseq2_batchcor <- readRDS(
+  file = here::here(
+    "input",
+    "txome",
+    "quant_deseq2_batchcor_nofilter.RDS"
+  )
+)
+txome_results <- readRDS(
+  file = here::here(
+    "input",
+    "txome",
+    "results_deseq2_nofilter.RDS"
+  )
+)
+
+# Subset data based on conditions and genes
+txome_quant_small <- txome_quant_deseq2_batchcor %$%
+  .[, colData(.)$condition_ID %in% txome_conditions_interest] %$%
+  .[, colData(.)$cell_line %in% c("hMSC-21558")]
+
+txome_genes_sel <- txome_quant_small %>%
+  rowRanges() %>%
+  as.data.frame() %>%
+  filter(symbol %in% mndata_genes) %>%
+  arrange(match(gene_name, mndata_genes))
+
+## Readjust DESeq results to include only GOIs
+txome_results_subset <- txome_results %>%
+  map(\(x) {
+    x <- x[rownames(x) %in% txome_genes_sel$gene_id, ]
+    x$padj <- p.adjust(x$pvalue, method = "BH")
+    return(x)
+  })
+
+# Define functions for drawing boxplots
+## Draw boxplot hMSC response to heparin (txome)
+txome_plot_goi <- function(gene_sel, gene_name) {
+  # Get table containing normalised gene counts
+  gene_counts <- txome_quant_small %>%
+    plotCounts(
+      gene = gene_sel,
+      intgroup = c("condition_ID", "Treatment"),
+      returnData = TRUE
+    ) %>%
+    droplevels()
+
+  # Get position to start drawing signif bars
+  y_position <- log10(max(gene_counts$count) * 1.1)
+
+  # Define position of bars and numbers manually
+
+  # Plot data
+  plot <- gene_counts %>%
+    ggplot(aes(
+      x = condition_ID,
+      y = count
+    )) +
+    geom_boxplot(
+      aes(
+        colour = condition_ID
+      ),
+      outliers = FALSE
+    ) +
+    geom_jitter(
+      aes(
+        fill = condition_ID,
+        shape = Treatment
+      ),
+      colour = "black",
+      size = 1.2,
+      stroke = 0.5,
+      alpha = 0.5
+    ) +
+    geom_signif(
+      comparisons = list(
+        c("P5D5Untreated", "P5D5Treated"),
+        c("P5D5Untreated", "P13D5Untreated"),
+        c("P13D5Untreated", "P13D5Treated")
+      ),
+      annotation = c(2, 21, 6) %>%
+        map(\(coef) txome_results_subset[[coef]][gene_sel, ]$padj) %>%
+        unlist() %>%
+        stars_pval(),
+      y_position = y_position %>%
+        rep(3),
+      textsize = 3,
+      step_increase = 0.2
+    ) +
+    scale_colour_manual(
+      values = palette_merge[c(1, 2, 5, 6)],
+      labels = c(
+        "P5D5Untreated" = "P+5 D5 Control",
+        "P5D5Treated" = "P+5 D5 Heparin",
+        "P13D5Untreated" = "P+13 D5 Control",
+        "P13D5Treated" = "P+13 D5 Heparin"
+      )
+    ) +
+    scale_fill_manual(
+      values = palette_merge[c(1, 2, 5, 6)],
+      labels = c(
+        "P5D5Untreated" = "P+5 D5 Control",
+        "P5D5Treated" = "P+5 D5 Heparin",
+        "P13D5Untreated" = "P+13 D5 Control",
+        "P13D5Treated" = "P+13 D5 Heparin"
+      )
+    ) +
+    scale_shape_manual(
+      values = c(
+        "Untreated" = 21,
+        "Treated" = 24
+      )
+    ) +
+    scale_x_discrete(
+      labels = c(
+        "P5D5Untreated" = "P+5 D5 Control",
+        "P5D5Treated" = "P+5 D5 Heparin",
+        "P13D5Untreated" = "P+13 D5 Control",
+        "P13D5Treated" = "P+13 D5 Heparin"
+      )
+    ) +
+    scale_y_log10(expand = expansion(0, 0.15)) +
+    theme_classic() +
+    ggtitle(label = gene_name) +
+    theme(
+      axis.text.x = element_blank(),
+      axis.ticks.x = element_blank()
+    ) +
+    ylab("Norm. counts")
+
+  # Return object
+  return(plot)
+}
+
+# Draw plots
+## Boxplots of norm counts
+txome_plots_goi <- txome_genes_sel %$%
+  map2(
+    .$gene_id,
+    .$gene_name,
+    \(x, y) txome_plot_goi(x, y)
+  ) %>%
+  set_names(txome_genes_sel$gene_name)
+
+# Save data
+saveRDS(
+  txome_plots_goi,
+  here::here(
+    "output",
+    "plots_box",
+    "data",
+    "txome_plots_goi.rds"
+  )
+)
